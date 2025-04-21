5544023d00046fa4d3032d73d3f4a7c927585868
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-21 11:51:48 +0800

    Revert "UserBackupSettingsActivity: Use correct content ID"
    This reverts commit bfb6a8cdb9960cc84581aa4b2d576c6eb9470f94.

--- src/com/android/settings/backup/UserBackupSettingsActivity.java
+++ src/com/android/settings/backup/UserBackupSettingsActivity.java
@@ -89,14 +89,14 @@ public class UserBackupSettingsActivity extends SettingsActivity implements Inde
                 Log.d(TAG, "Manufacturer provided backup settings, showing the preference screen");
             }
 
-            InsetUtils.applyWindowInsetsListener(findViewById(android.R.id.content));
+            InsetUtils.applyWindowInsetsListener(findViewById(R.id.main_content));
 
             // mFragmentManager can be set by {@link #setFragmentManager()} for testing
             if (mFragmentManager == null) {
                 mFragmentManager = getSupportFragmentManager();
             }
             mFragmentManager.beginTransaction()
-                    .replace(android.R.id.content, new BackupSettingsFragment())
+                    .replace(R.id.main_content, new BackupSettingsFragment())
                     .commit();
         }
     }
